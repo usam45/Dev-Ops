da296f0d02eef9c999b3b462c7db53f94f08a2f6
usama.arshad <usama.arshad@nitb.gov.pk>
2023-04-10 12:22:05 +0500

    test casess

--- MyFirstAzureWebAppTests/IndexModelTests.cs
+++ MyFirstAzureWebAppTests/IndexModelTests.cs
@@ -1,12 +1,43 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyFirstAzureWebApp.Pages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MyFirstAzureWebAppTests
+namespace MyFirstAzureWebApp.Pages.Tests
 {
-	internal class IndexModelTests
+	[TestClass()]
+	public class IndexModelTests
 	{
+		private readonly IndexModel _indexModel;
+		public IndexModelTests(IndexModel indexModel)
+		{
+			_indexModel = indexModel;
+		}
+
+		[TestMethod()]
+		public void addTest()
+		{
+			Assert.Fail();
+		}
+		public void Add_ByAddingTwoNumbers_returns_sum_of_numbers()
+		{
+			//Arrange
+			var a = 4;
+			var b = 5;
+			//Act
+			var result = _indexModel.add(a, b);
+			//Assert
+			Assert.AreEqual(9, result);
+		}
+
+		[TestMethod]
+		public void Add_ByAddingTwoNumbers_returns_sum_of_numbers_smart()
+		{
+			Assert.AreEqual(13, _indexModel.add(6, 7));
+		}
 	}
+
 }
