e6112aac9ebecb2e14d32056f0b581bc11d96a71
usam45 <103037415+usam45@users.noreply.github.com>
2023-04-11 09:12:14 +0500

    Update Index.cshtml.cs

--- MyFirstAzureWebApp/Pages/Index.cshtml.cs
+++ MyFirstAzureWebApp/Pages/Index.cshtml.cs
@@ -6,10 +6,13 @@ namespace MyFirstAzureWebApp.Pages;
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    private readonly IConfiguration _configuration;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(ILogger<IndexModel> logger,
+        IConfiguration configuration)
     {
         _logger = logger;
+        _configuration = configuration;
     }
 
     public void OnGet()
